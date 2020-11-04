29505ac0fa153e5a371ebb30a1272f477176d7fd
Ramya.U <ramyayadav2016@gmail.com>
2020-11-04 13:24:58 +0530

    Add project files.

---
+++ FindMaxNumberDemo/MaximumNumberCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxNumberDemo
+{
+    public class MaximumNumberCheck
+    {
+            public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+            {
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+                {
+                    return thirdValue;
+                }
+                throw new Exception("all the numbers have same value");
+            }
+            public static double MaximumDoubleNumber(double firstValue, double secondValue, double thirdValue)
+            {
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+                {
+                    return thirdValue;
+                }
+                throw new Exception("all the numbers have same value");
+            }
+        }
+    }
---
+++ FindMaxNumberDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindMaxNumberDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the max number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+            double output1 = MaximumNumberCheck.MaximumDoubleNumber(11.1, 22.2, 33.3);
+            Console.WriteLine(output1);
+        }
+    }
+}
